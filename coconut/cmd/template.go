1bba130fb72f6a0c01c47b2acf1755e21ce405e3
Teo Mrnjavac <teo.mrnjavac@cern.ch>
2018-12-05 15:58:10 +0100

    [coconut] Implement list workflow templates

---
+++ coconut/cmd/template.go
@@ -0,0 +1,45 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2018 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/AliceO2Group/Control/common/product"
+	"github.com/spf13/cobra"
+)
+
+// templateCmd represents the template command
+var templateCmd = &cobra.Command{
+	Use:   "template",
+	Aliases: []string{"templ"},
+	Short: "query available workflow templates in O² configuration",
+	Long: fmt.Sprintf(`The template command interacts with the loaded %s configuration to
+display information on available O² templates.`, product.PRETTY_SHORTNAME),
+}
+
+func init() {
+	rootCmd.AddCommand(templateCmd)
+}
