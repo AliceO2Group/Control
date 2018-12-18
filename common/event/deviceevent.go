a51581502535e0293b23fc7fbf43b80eac5f1536
Teo Mrnjavac <teo.m@cern.ch>
2018-12-18 16:00:44 +0100

    [common] Add event.DeviceEvent

---
+++ common/event/deviceevent.go
@@ -0,0 +1,86 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2018 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+package event
+
+import (
+	"github.com/AliceO2Group/Control/executor/protos"
+	"github.com/mesos/mesos-go/api/v1/lib"
+)
+
+type DeviceEventOrigin struct {
+	AgentId      mesos.AgentID      `json:"agentId"`
+	ExecutorId   mesos.ExecutorID   `json:"executorId"`
+	TaskId       mesos.TaskID       `json:"taskId"`
+}
+
+type DeviceEvent interface {
+	GetName() string
+	GetOrigin() DeviceEventOrigin
+	GetType() pb.DeviceEventType
+}
+
+type DeviceEventBase struct {
+	Type        pb.DeviceEventType       `json:"type"`
+	Origin      DeviceEventOrigin        `json:"origin"`
+	MessageType string                   `json:"_messageType"`
+}
+
+func (b *DeviceEventBase) GetOrigin() DeviceEventOrigin {
+	if b == nil {
+		return DeviceEventOrigin{}
+	}
+	return b.Origin
+}
+
+func (b *DeviceEventBase) GetType() pb.DeviceEventType {
+	if b == nil {
+		return pb.DeviceEventType_NULL_DEVICE_EVENT
+	}
+	return b.Type
+}
+
+func NewDeviceEvent(origin DeviceEventOrigin, t pb.DeviceEventType) (de DeviceEvent) {
+	switch t {
+	case pb.DeviceEventType_END_OF_DATA:
+		de = &EndOfData{
+			DeviceEventBase: DeviceEventBase{
+				Type:   t,
+				Origin: origin,
+				MessageType: "DeviceEvent",
+			},
+		}
+	case pb.DeviceEventType_NULL_DEVICE_EVENT:
+		de = nil
+	}
+	return de
+}
+
+type EndOfData struct {
+	DeviceEventBase
+}
+
+func (e *EndOfData) GetName() string {
+	return "END_OF_DATA"
+}
