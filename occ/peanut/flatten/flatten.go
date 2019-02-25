9ad32c1c5f5dab2801a2e2acd49028d6ab763a7b
Teo Mrnjavac <teo.m@cern.ch>
2019-02-25 12:26:46 +0100

    [peanut] YAML-ified flatten package to facilitate config generation

---
+++ occ/peanut/flatten/flatten.go
@@ -0,0 +1,218 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2019 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+// Portions originally from https://github.com/jeremywohl/flatten
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Jeremy Wohl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// Flatten makes flat, one-dimensional maps from arbitrarily nested ones.
+//
+// It turns map keys into compound
+// names, in four styles: dotted (`a.b.1.c`), path-like (`a/b/1/c`), Rails (`a[b][1][c]`), or with underscores (`a_b_1_c`).  It takes input as either JSON strings or
+// Go structures.  It knows how to traverse these JSON types: objects/maps, arrays and scalars.
+//
+// You can flatten JSON strings.
+//
+//	nested := `{
+//	  "one": {
+//	    "two": [
+//	      "2a",
+//	      "2b"
+//	    ]
+//	  },
+//	  "side": "value"
+//	}`
+//
+//	flat, err := flatten.FlattenString(nested, "", flatten.DotStyle)
+//
+//	// output: `{ "one.two.0": "2a", "one.two.1": "2b", "side": "value" }`
+//
+// Or Go maps directly.
+//
+//	nested := map[string]interface{}{
+//		"a": "b",
+//		"c": map[string]interface{}{
+//			"d": "e",
+//			"f": "g",
+//		},
+//		"z": 1.4567,
+//	}
+//
+//	flat, err := flatten.Flatten(nested, "", flatten.RailsStyle)
+//
+//	// output:
+//	// map[string]interface{}{
+//	//	"a":    "b",
+//	//	"c[d]": "e",
+//	//	"c[f]": "g",
+//	//	"z":    1.4567,
+//	// }
+//
+package flatten
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+
+	"gopkg.in/yaml.v2"
+)
+
+// The presentation style of keys.
+type SeparatorStyle int
+
+const (
+	_ SeparatorStyle = iota
+
+	// Separate nested key components with dots, e.g. "a.b.1.c.d"
+	DotStyle
+
+	// Separate with path-like slashes, e.g. a/b/1/c/d
+	PathStyle
+
+	// Separate ala Rails, e.g. "a[b][c][1][d]"
+	RailsStyle
+
+	// Separate with underscores, e.g. "a_b_1_c_d"
+	UnderscoreStyle
+)
+
+// Nested input must be a map or slice
+var NotValidInputError = errors.New("Not a valid input: map or slice")
+
+// Flatten generates a flat map from a nested one.  The original may include values of type map, slice and scalar,
+// but not struct.  Keys in the flat map will be a compound of descending map keys and slice iterations.
+// The presentation of keys is set by style.  A prefix is joined to each key.
+func Flatten(nested map[string]interface{}, prefix string, style SeparatorStyle) (map[string]interface{}, error) {
+	flatmap := make(map[string]interface{})
+
+	err := flatten(true, flatmap, nested, prefix, style)
+	if err != nil {
+		return nil, err
+	}
+
+	return flatmap, nil
+}
+
+// FlattenString generates a flat JSON map from a nested one.  Keys in the flat map will be a compound of
+// descending map keys and slice iterations.  The presentation of keys is set by style.  A prefix is joined
+// to each key.
+func FlattenString(nestedstr, prefix string, style SeparatorStyle) (string, error) {
+	var nested map[string]interface{}
+	err := yaml.Unmarshal([]byte(nestedstr), &nested)
+	if err != nil {
+		return "", err
+	}
+
+	flatmap, err := Flatten(nested, prefix, style)
+	if err != nil {
+		return "", err
+	}
+
+	flatb, err := json.MarshalIndent(&flatmap, "", "  ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(flatb), nil
+}
+
+func flatten(top bool, flatMap map[string]interface{}, nested interface{}, prefix string, style SeparatorStyle) error {
+	assign := func(newKey string, v interface{}) error {
+		switch v.(type) {
+		case map[string]interface{}, []interface{}, map[interface{}]interface{}:
+			if err := flatten(false, flatMap, v, newKey, style); err != nil {
+				return err
+			}
+		default:
+			flatMap[newKey] = v
+		}
+
+		return nil
+	}
+
+	switch nested.(type) {
+	case map[interface{}]interface{}:
+		for k, v := range nested.(map[interface{}]interface{}) {
+			newKey := enkey(top, prefix, k.(string), style)
+			assign(newKey, v)
+		}
+	case map[string]interface{}:
+		for k, v := range nested.(map[string]interface{}) {
+			newKey := enkey(top, prefix, k, style)
+			assign(newKey, v)
+		}
+	case []interface{}:
+		for i, v := range nested.([]interface{}) {
+			newKey := enkey(top, prefix, strconv.Itoa(i), style)
+			assign(newKey, v)
+		}
+	default:
+		return NotValidInputError
+	}
+
+	return nil
+}
+
+func enkey(top bool, prefix, subkey string, style SeparatorStyle) string {
+	key := prefix
+
+	if top {
+		key += subkey
+	} else {
+		switch style {
+		case DotStyle:
+			key += "." + subkey
+		case PathStyle:
+			key += "/" + subkey
+		case RailsStyle:
+			key += "[" + subkey + "]"
+		case UnderscoreStyle:
+			key += "_" + subkey
+		}
+	}
+
+	return key
+}
