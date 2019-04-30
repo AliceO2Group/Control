ae13a7e244d455944853c72441ab0fbdc0a9ea07
Teo Mrnjavac <teo.m@cern.ch>
2019-04-30 11:22:12 +0200

    [misc] Make release notes script usable against HEAD

--- hacking/release_notes.sh
+++ hacking/release_notes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-previous_tag=$(git describe --tags --abbrev=0 v$1^)
-echo "Commits from $previous_tag to v$1:"
-git log $previous_tag..v$1 --oneline --format="%s" --reverse|sort -s -k1.1,1.4
+previous_tag=$(git describe --tags --abbrev=0 $1^)
+echo "Commits from $previous_tag to $1:"
+git log $previous_tag..$1 --oneline --format="%s" --reverse|sort -s -k1.1,1.4
