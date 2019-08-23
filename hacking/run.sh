1a13cd4bd2ff1ba4bf489c69f568d403e7551116
Teo Mrnjavac <teo.m@cern.ch>
2019-08-23 13:55:22 +0200

    [misc] Update default configuration

--- hacking/run.sh
+++ hacking/run.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #parent dir of this scri
 DIR="$(dirname "$DIR")"                                 #up one level
 cd $DIR
 
-bin/o2control-core --mesosUrl http://m1.dcos:5050/api/v1/scheduler --executor /vagrant/go/src/github.com/AliceO2Group/Control/bin/o2control-executor --verbose --globalConfigurationUri "file://hacking/config.yaml" --veryVerbose
+#bin/o2control-core --mesosUrl http://aidrefsrv21.cern.ch:5050/api/v1/scheduler --executor /opt/alisw/el7/Control-Core/v0.9.4-1/bin/o2control-executor --verbose --coreConfigurationUri "file://hacking/config.yaml" --veryVerbose
+bin/o2control-core --coreConfigurationUri "file://hacking/settings.yaml"
 #bin/o2control-core --coreConfigurationUri file://hacking/settings.yaml.example
 #bin/o2control-core --coreConfigurationUri consul://aido2cnf01:8500/test/kostas/o2/aliecs/settingsv4
