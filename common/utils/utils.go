bc4af35506b0e53de9410fe41111a29f2baeca91
Teo Mrnjavac <teo.m@cern.ch>
2019-12-12 09:35:44 +0100

    [misc] Add protoString utility function to utils

--- common/utils/utils.go
+++ common/utils/utils.go
@@ -1,3 +1,27 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2018-2019 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
 package utils
 
 import (
@@ -26,3 +50,6 @@ func EnsureTrailingSlash(path *string) {
 		*path += "/"
 	}
 }
+
+// helper func to package strings up nicely for protobuf
+func ProtoString(s string) *string { return &s }
