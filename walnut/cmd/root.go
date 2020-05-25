6ab47e87795e6ccb7c5b97828478e0329c5b5f1f
Ayaan Zaidi <biblioklept@icloud.com>
2020-05-26 02:03:32 +0530

    [walnut] minor review corrections

--- walnut/cmd/root.go
+++ walnut/cmd/root.go
@@ -65,7 +65,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.walnut.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", fmt.Sprintf("optional configuration file for %s (default $HOME/.config/%s/settings.yaml)", app.NAME, app.NAME))
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
