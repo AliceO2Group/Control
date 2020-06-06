85e72fa0614e402340c6dde6b5788819b36b02f1
Ayaan Zaidi <biblioklept@icloud.com>
2020-06-06 19:13:11 +0530

    [walnut] Init importer

---
+++ walnut/converter/importer_test.go
@@ -0,0 +1,36 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2020 CERN and copyright holders of ALICE O².
+ * Author: Ayaan Zaidi <azaidi@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+package converter
+
+import "testing"
+
+func TestImportUnmarshal(t *testing.T) {
+	t.Run("Testing dump reading", func(t *testing.T) {
+		err := importer("dump.json")
+		if err != nil {
+			t.Errorf("import failed: %v", err)
+		}
+	})
+}
