bada8c8b1aaf2b8af5942d2b29c08dd93d37a126
Ayaan Zaidi <biblioklept@icloud.com>
2020-06-22 01:41:39 +0530

    [walnut] removed log handling from workflow importer

--- walnut/converter/importer_test.go
+++ walnut/converter/importer_test.go
@@ -36,7 +36,6 @@ func TestImportUnmarshal(t *testing.T) {
 		if err != nil {
 			t.Errorf("Opening file failed: %w", err)
 		}
-		defer file.Close()
 
 		DplDump, err := jsonImporter(file)
 		if err != nil {
