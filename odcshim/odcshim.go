5121d630dd5e740ed1ebf70a91e6b44dc1331261
Teo Mrnjavac <teo.m@cern.ch>
2020-12-15 03:12:05 +0100

    [core] Migrate from gogo/protobuf to protobuf generator v2

--- odcshim/odcshim.go
+++ odcshim/odcshim.go
@@ -36,8 +36,8 @@ import (
 	"github.com/spf13/viper"
 )
 
-//go:generate protoc -I ../occ     --gofast_out=plugins=grpc:. protos/occ.proto
-//go:generate protoc -I ../odcshim --gofast_out=plugins=grpc:. odcprotos/odc.proto
+//go:generate protoc -I ../occ     --go_out=plugins=grpc:. protos/occ.proto
+//go:generate protoc -I ../odcshim --go_out=plugins=grpc:. odcprotos/odc.proto
 
 var log = logger.New(logrus.StandardLogger(), "o2-aliecs-odc-shim")
 
