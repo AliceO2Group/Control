ada65457102818a65867a03f8a7bc0ca53ddc268
Teo Mrnjavac <teo.m@cern.ch>
2021-01-18 14:09:49 +0100

    [executor] stderr output of controlled processes now forwarded at DebugLevel
    OCTRL-362 #close

--- executor/executable/controllabletask.go
+++ executor/executable/controllabletask.go
@@ -120,7 +120,7 @@ func (t *ControllableTask) Launch() error {
 			_, errStdout = io.Copy(log.WithPrefix("task-stdout").WithField("task", t.ti.Name).WriterLevel(logrus.DebugLevel), stdoutIn)
 		}()
 		go func() {
-			_, errStderr = io.Copy(log.WithPrefix("task-stderr").WithField("task", t.ti.Name).WriterLevel(logrus.ErrorLevel), stderrIn)
+			_, errStderr = io.Copy(log.WithPrefix("task-stderr").WithField("task", t.ti.Name).WriterLevel(logrus.DebugLevel), stderrIn)
 		}()
 
 		log.WithFields(logrus.Fields{
