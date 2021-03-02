b402da4f59dd28c906083048be4d6bdb35c4f107
Teo Mrnjavac <teo.m@cern.ch>
2021-03-02 03:52:47 +0100

    [misc] Regenerate protofiles

--- odcshim/protos/occ_grpc.pb.go
+++ odcshim/protos/occ_grpc.pb.go
@@ -11,7 +11,6 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // OccClient is the client API for Occ service.
@@ -35,7 +34,7 @@ func NewOccClient(cc grpc.ClientConnInterface) OccClient {
 }
 
 func (c *occClient) EventStream(ctx context.Context, in *EventStreamRequest, opts ...grpc.CallOption) (Occ_EventStreamClient, error) {
-	stream, err := c.cc.NewStream(ctx, &Occ_ServiceDesc.Streams[0], "/occ_pb.Occ/EventStream", opts...)
+	stream, err := c.cc.NewStream(ctx, &_Occ_serviceDesc.Streams[0], "/occ_pb.Occ/EventStream", opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -67,7 +66,7 @@ func (x *occEventStreamClient) Recv() (*EventStreamReply, error) {
 }
 
 func (c *occClient) StateStream(ctx context.Context, in *StateStreamRequest, opts ...grpc.CallOption) (Occ_StateStreamClient, error) {
-	stream, err := c.cc.NewStream(ctx, &Occ_ServiceDesc.Streams[1], "/occ_pb.Occ/StateStream", opts...)
+	stream, err := c.cc.NewStream(ctx, &_Occ_serviceDesc.Streams[1], "/occ_pb.Occ/StateStream", opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -153,7 +152,7 @@ type UnsafeOccServer interface {
 }
 
 func RegisterOccServer(s grpc.ServiceRegistrar, srv OccServer) {
-	s.RegisterService(&Occ_ServiceDesc, srv)
+	s.RegisterService(&_Occ_serviceDesc, srv)
 }
 
 func _Occ_EventStream_Handler(srv interface{}, stream grpc.ServerStream) error {
@@ -234,10 +233,7 @@ func _Occ_Transition_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
-// Occ_ServiceDesc is the grpc.ServiceDesc for Occ service.
-// It's only intended for direct use with grpc.RegisterService,
-// and not to be introspected or modified (even as a copy)
-var Occ_ServiceDesc = grpc.ServiceDesc{
+var _Occ_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "occ_pb.Occ",
 	HandlerType: (*OccServer)(nil),
 	Methods: []grpc.MethodDesc{
