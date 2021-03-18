a72bdd3535ea04edfb525aaea603c3f8c5dc8d34
Claire Guyot <claireiere33@gmail.com>
2021-03-18 18:52:15 +0100

    First uncomplete attempt at creating a Web Api for Apricot.

--- apricot/local/service.go
+++ apricot/local/service.go
@@ -29,6 +29,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -42,6 +43,7 @@ import (
 	"github.com/AliceO2Group/Control/configuration/componentcfg"
 	"github.com/AliceO2Group/Control/configuration/template"
 	"github.com/flosch/pongo2/v4"
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -52,9 +54,96 @@ type Service struct {
 	src cfgbackend.Source
 }
 
+type machineInfo struct {
+	name string
+}
+
+type clusterInfo struct {
+	FLPs []machineInfo
+}
+
+// Just a skeleton of a function for getting FLP names if relevant
+/*func queryMachines(cluster *clusterInfo) error {
+	for rows.Next() {
+		machine := machineInfo{}
+		err = rows.Scan(&machineInfo.name)
+		if err != nil {
+			return err
+		}
+		cluster.FLPs = append(cluster.FLPs, machine)
+	}
+	err = rows.Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}*/
+
+func ApiGetClusterInformation(w http.ResponseWriter, r *http.Request) {
+	queryParam := mux.Vars(r)
+    format := ""
+    var err error
+	format, err = queryParam["format"]
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error, format is not readable.")
+		return
+	}
+	switch format {
+	case "json":
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		//TODO: write JSON
+		/* where to get info? depending on answer, could proceed like:
+		
+		cluster := clusterInfo{}
+		err := queryMachines(&cluster)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error, no result in cluster.")
+		}
+		answer, err := json.Marshal(cluster)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error, result could not be given.")
+		}
+		fmt.Fprintf(w, string(answer))
+		*/
+	case "text":
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		//TODO: write plain text
+		/* something like: ???
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+		*/
+	default: 
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error, format should be json or text.")
+	}
+}
+
+func ApiUnhandledRequest(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Request not handled. Request should be of type GET.")
+}
+
+func ApiRequestNotFound(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Request not found.")
+}
+
 func NewService(uri string) (svc *Service, err error) {
 	var src cfgbackend.Source
 	src, err = cfgbackend.NewSource(uri)
+	router := mux.NewRouter()
+	webApi := router.PathPrefix("/inventory/flps").Subrouter()
+    webApi.HandleFunc("/{format}", ApiGetClusterInformation).Methods(http.MethodGet)
+	webApi.HandleFunc("", ApiUnhandledRequest).Methods(http.MethodPost)
+	webApi.HandleFunc("", ApiUnhandledRequest).Methods(http.MethodPut)
+	webApi.HandleFunc("", ApiUnhandledRequest).Methods(http.MethodDelete)
+	webApi.HandleFunc("", ApiRequestNotFound)
+	log.WithError(http.ListenAndServe(uri, router)).Fatal("Fatal error with Web API.")
 	return &Service{
 		src: src,
 	}, err
