c605f665eba873a1b8a12d3214daf6626ca78de9
Miltiadis Alexis <alexmiltiadis@gmail.com>
2021-04-20 16:10:20 +0200

    [core] Retrieve JWT using the request redirect

--- core/bookkeeping/api.go
+++ core/bookkeeping/api.go
@@ -22,52 +22,46 @@
  * Intergovernmental Organization or submit itself to any jurisdiction.
  */
 
-// This package instantiates the bookkeeping API client. There is one
-// issue with the local import in github.com/AliceO2Group/Bookkeeping/go-api-client/src
-// which should be fixed, or otherwise we need to mirror the BookkeepingAPI.go here.
-// The other possible issue is that currently all the call instead of returning the
-// responses and the errors they use fmt.Println(). This can be solved again by mirroring,
-// and changing the implementation of the BookkeepingAPI.go to return instead of printing,
-// but this is not a valid solution since we have to reflect every change that might occur
-// on the file.
 package bookkeeping
 
 import (
+	"net/url"
+	"path"
 	"sync"
 
 	clientAPI "github.com/AliceO2Group/Bookkeeping/go-api-client/src"
 	sw "github.com/AliceO2Group/Bookkeeping/go-api-client/src/go-client-generated"
+	"github.com/AliceO2Group/Control/common/logger"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-const (
-	bookkeepingBaseUrl       = "http://vm4.jiskefet.io/api"
-	// APItoken provided doesn't work, we should contact team on how to generate one.
-	bookkeepingAPIToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzQ2NjQwLCJ1c2VybmFtZSI6ImF3ZWdyenluIiwiYWNjZXNzIjoxLCJpYXQiOjE2MTA1NDM0ODYsImV4cCI6MTYxMDYyOTg4NiwiaXNzIjoibzItdWkifQ.8pM1K0HIfpnZop7bJk_rD5GvkfeiWaNNs2S7ZM1tqYg"
-)
+var log = logger.New(logrus.StandardLogger(), "Bookkeeping")
 
-type BookkeepingWrapper struct {}
+type BookkeepingWrapper struct{}
 
 var (
 	once sync.Once
 	// mock instance
 	instance *BookkeepingWrapper
 )
+
 func Instance() *BookkeepingWrapper {
 	once.Do(func() {
-		clientAPI.InitializeApi(bookkeepingBaseUrl, bookkeepingAPIToken)
+		apiUrl, err  := url.Parse(viper.GetString("bookkeepingBaseUri"))
+		if err == nil {
+			apiUrl.Path = path.Join( apiUrl.Path + "api")
+			clientAPI.InitializeApi(apiUrl.String(), getJWTAPIToken())
+		} else {
+			log.WithField("error", err.Error()).
+			Error("unable to parse the Bookkeeping base URL")
+			clientAPI.InitializeApi(viper.GetString("bookkeepingBaseUri")+"/api", getJWTAPIToken())
+		}
 	})
 	return instance
 }
 
-func initializeBookkeepingAPI() *BookkeepingWrapper {
-	var bkw BookkeepingWrapper
-	clientAPI.InitializeApi(bookkeepingBaseUrl, bookkeepingAPIToken)
-	return &bkw
-
-}
-
-
-func (bk *BookkeepingWrapper) CreateRun(activityId string, nDetectors int, nEpns int, nFlps int, runNumber int, runType string, timeO2Start int64, timeTrgStart int64){
+func (bk *BookkeepingWrapper) CreateRun(activityId string, nDetectors int, nEpns int, nFlps int, runNumber int, runType string, timeO2Start int64, timeTrgStart int64) {
 	var runtypeAPI sw.RunType
 	switch runType {
 	case string(sw.TECHNICAL_RunType):
@@ -81,11 +75,10 @@ func (bk *BookkeepingWrapper) CreateRun(activityId string, nDetectors int, nEpns
 		runtypeAPI = sw.TECHNICAL_RunType
 	}
 
-	clientAPI.CreateRun(activityId, int64(nDetectors),int64(nEpns),int64(nFlps),int64(runNumber), runtypeAPI, timeO2Start, timeTrgStart)
-
+	clientAPI.CreateRun(activityId, int64(nDetectors), int64(nEpns), int64(nFlps), int64(runNumber), runtypeAPI, timeO2Start, timeTrgStart)
 }
 
-func (bk *BookkeepingWrapper) UpdateRun(runNumber int, runResult string, timeO2End int64, timeTrgEnd int64){
+func (bk *BookkeepingWrapper) UpdateRun(runNumber int, runResult string, timeO2End int64, timeTrgEnd int64) {
 	var runquality sw.RunQuality
 	switch runResult {
 	case string(sw.GOOD_RunQuality):
@@ -102,23 +95,22 @@ func (bk *BookkeepingWrapper) UpdateRun(runNumber int, runResult string, timeO2E
 	clientAPI.UpdateRun(int64(runNumber), runquality, timeO2End, timeTrgEnd)
 }
 
-
-func (bk *BookkeepingWrapper) CreateLog(text string, title string, runNumbers string, parentLogId int64){
+func (bk *BookkeepingWrapper) CreateLog(text string, title string, runNumbers string, parentLogId int64) {
 	clientAPI.CreateLog(text, title, runNumbers, parentLogId)
 }
 
-func (bk *BookkeepingWrapper) CreateFlp(name string, hostName string, runNumber int64 ){
+func (bk *BookkeepingWrapper) CreateFlp(name string, hostName string, runNumber int64) {
 	clientAPI.CreateFlp(name, hostName, runNumber)
 }
 
-func (bk *BookkeepingWrapper) UpdateFlp(flpId int64, name string, nSubtimeframes int64, nEquipmentBytes int64, nRecordingBytes int64, nFairMQBytes int64){
+func (bk *BookkeepingWrapper) UpdateFlp(flpId int64, name string, nSubtimeframes int64, nEquipmentBytes int64, nRecordingBytes int64, nFairMQBytes int64) {
 	clientAPI.UpdateFlp(flpId, name, nSubtimeframes, nEquipmentBytes, nRecordingBytes, nFairMQBytes)
 }
 
-func (bk *BookkeepingWrapper) GetLogs(){
+func (bk *BookkeepingWrapper) GetLogs() {
 	clientAPI.GetLogs()
 }
 
-func (bk *BookkeepingWrapper) GetRuns(){
+func (bk *BookkeepingWrapper) GetRuns() {
 	clientAPI.GetRuns()
 }
