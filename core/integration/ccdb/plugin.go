7cbe5e9f4e19fd77e5a22ecc7692f1d1ec371a74
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-02-14 10:31:21 +0100

    correct author

--- core/integration/ccdb/plugin.go
+++ core/integration/ccdb/plugin.go
@@ -2,7 +2,7 @@
  * === This file is part of ALICE O² ===
  *
  * Copyright 2021 CERN and copyright holders of ALICE O².
- * Author: Miltiadis Alexis <miltiadis.alexis@cern.ch>
+ * Author: Piotr Konopka <piotr.jan.konopka@cern.ch>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
