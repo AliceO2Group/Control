db5f120c93d86be381ae6a80d209c22cf1323a47
claireguyot <claireiere33@gmail.com>
2022-07-06 13:25:41 +0200

    [O2B-560] A Bookkeeping run is created as early as possible with as many parameters as available, and updated afterwards with the necessary information.

--- core/integration/bookkeeping/plugin.go
+++ core/integration/bookkeeping/plugin.go
@@ -179,15 +179,10 @@ func (p *Plugin) CallStack(data interface{}) (stack map[string]interface{}) {
 		epn_enabled, _ := strconv.ParseBool(env.GetKV("", "epn_enabled"))
 		odc_topology := env.GetKV("", "odc_topology")
 		detectors := strings.Join(env.GetActiveDetectors().StringList(), ",")
-		odc_topology_fullname, _ := env.Workflow().GetVars().Get("odc_topology_fullname")
-		trg_global_run_enabled, _ := strconv.ParseBool(env.GetKV("", "trg_global_run_enabled"))
-		trg_enabled, _ := strconv.ParseBool(env.GetKV("", "trg_enabled"))
-		//pdp_config_option := env.GetKV("", "pdp_config_option")
-		//pdp_topology_description_library_file := env.GetKV("", "pdp_topology_description_library_file")
-		tfb_dd_mode := env.GetKV("", "tfb_dd_mode")
-		lhc_period := env.GetKV("", "lhc_period")
+		//odc_topology_fullname, _ := env.Workflow().GetVars().Get("odc_topology_fullname")
+		//lhc_period := env.GetKV("", "lhc_period")
 
-		err = p.bookkeepingClient.CreateRun(env.Id().String(), len(env.GetActiveDetectors()), 0, len(flps), int32(runNumber), env.GetRunType().String(), time.Now(), time.Now(), dd_enabled, dcs_enabled, epn_enabled, odc_topology, detectors, odc_topology_fullname, trg_global_run_enabled, trg_enabled, tfb_dd_mode, lhc_period)
+		err = p.bookkeepingClient.CreateRun(env.Id().String(), len(env.GetActiveDetectors()), 0, len(flps), int32(runNumber), env.GetRunType().String(), dd_enabled, dcs_enabled, epn_enabled, odc_topology, detectors)
 		if err != nil {
 			log.WithError(err).
 				WithField("runNumber", runNumber).
@@ -241,9 +236,16 @@ func (p *Plugin) CallStack(data interface{}) (stack map[string]interface{}) {
 		}
 		return
 	}
-	updateRunFunc := func(runNumber64 int64, state string) (out string) {
+	updateRunFunc := func(runNumber64 int64, state string, timeO2Start time.Time, timeO2End time.Time, timeTrgStart time.Time, timeTrgEnd time.Time) (out string) {
 		callFailedStr := "Bookkeeping UpdateRun call failed"
-		err := p.bookkeepingClient.UpdateRun(int32(runNumber64), state, time.Now(), time.Now())
+		//odc_topology_fullname, _ := env.Workflow().GetVars().Get("odc_topology_fullname")
+		trg_global_run_enabled, _ := strconv.ParseBool(env.GetKV("", "trg_global_run_enabled"))
+		trg_enabled, _ := strconv.ParseBool(env.GetKV("", "trg_enabled"))
+		pdp_config_option, _ := varStack["pdp_config_option"]
+		pdp_topology_description_library_file, _ := varStack["pdp_topology_description_library_file"]
+		tfb_dd_mode := env.GetKV("", "tfb_dd_mode")
+		//lhc_period := env.GetKV("", "lhc_period")
+		err := p.bookkeepingClient.UpdateRun(int32(runNumber64), state, timeO2Start, timeO2End, timeTrgStart, timeTrgEnd, trg_global_run_enabled, trg_enabled, pdp_config_option, pdp_topology_description_library_file, tfb_dd_mode /*, odc_topology_fullname, lhc_period */)
 		if err != nil {
 			log.WithError(err).
 				WithField("runNumber", runNumber64).
@@ -293,7 +295,7 @@ func (p *Plugin) CallStack(data interface{}) (stack map[string]interface{}) {
 
 		envState := env.CurrentState()
 		if envState != "RUNNING" {
-			return updateRunFunc(runNumber64, "bad")
+			return updateRunFunc(runNumber64, "bad", time.Now(), time.Now(), time.Time{}, time.Time{})
 		} else {
 			return
 		}
@@ -329,9 +331,9 @@ func (p *Plugin) CallStack(data interface{}) (stack map[string]interface{}) {
 
 		envState := env.CurrentState()
 		if envState != "CONFIGURED" {
-			return updateRunFunc(runNumber64, "bad")
+			return updateRunFunc(runNumber64, "bad", time.Time{}, time.Time{}, time.Now(), time.Now())
 		} else {
-			return updateRunFunc(runNumber64, "good")
+			return updateRunFunc(runNumber64, "good", time.Time{}, time.Time{}, time.Now(), time.Now())
 		}
 	}
 	// Environment related Bookkeeping functions
