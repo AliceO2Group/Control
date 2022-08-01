9a9e54b9cd00616822388ff83af36c671809230e
Teo Mrnjavac <teo.m@cern.ch>
2022-08-01 11:10:20 +0200

    [common] DeviceEvents can now carry Labels, including environmentId

---
+++ common/labels.go
@@ -0,0 +1,44 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2022 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+package common
+
+import "github.com/AliceO2Group/Control/common/utils/uid"
+
+type Labeler interface {
+	GetLabels() map[string]string
+}
+
+func GetEnvironmentIdFromLabelerType(labeler Labeler) uid.ID {
+	if labeler != nil && len(labeler.GetLabels()) > 0 {
+		envIdS, ok := labeler.GetLabels()["environmentId"]
+		if ok {
+			envId, err := uid.FromString(envIdS)
+			if err == nil {
+				return envId
+			}
+		}
+	}
+	return uid.NilID()
+}
