6afcd1bc3a5573fe9ab47da6f8abe323d800f844
Michal Tichák <michal.tichak@cern.ch>
2024-02-22 13:25:06 +0100

    [kaki] simple example with docker-compose to spin up kafka locally

--- kaki/src/main.cpp
+++ kaki/src/main.cpp
@@ -1,5 +1,67 @@
-#include <InfoLogger/InfoLogger.hxx>
-#include <iostream>
-#include <kafka/KafkaClient.h>
+#include <kafka/KafkaConsumer.h>
 
-int main() { std::cout << "hiiiiiiiiii\n"; }
+#include <cstdlib>
+#include <iostream>
+#include <signal.h>
+#include <string>
+
+std::atomic_bool running = {true};
+
+void stopRunning(int sig) {
+  if (sig != SIGINT)
+    return;
+
+  if (running) {
+    running = false;
+  } else {
+    // Restore the signal handler, -- to avoid stuck with this handler
+    signal(SIGINT, SIG_IGN); // NOLINT
+  }
+}
+
+int main() {
+  using namespace kafka;
+  using namespace kafka::clients::consumer;
+
+  // Use Ctrl-C to terminate the program
+  signal(SIGINT, stopRunning); // NOLINT
+
+  const std::string brokers = "127.0.0.1:9092";
+  const Topic topic = "example-topic";
+
+  // Prepare the configuration
+  const Properties props({{"bootstrap.servers", {brokers}}});
+
+  // Create a consumer instance
+  KafkaConsumer consumer(props);
+
+  // Subscribe to topics
+  consumer.subscribe({topic});
+
+  while (running) {
+    // Poll messages from Kafka brokers
+    auto records = consumer.poll(std::chrono::milliseconds(100));
+
+    for (const auto &record : records) {
+      if (!record.error()) {
+        std::cout << "Got a new message..." << std::endl;
+        std::cout << "    Topic    : " << record.topic() << std::endl;
+        std::cout << "    Partition: " << record.partition() << std::endl;
+        std::cout << "    Offset   : " << record.offset() << std::endl;
+        std::cout << "    Timestamp: " << record.timestamp().toString()
+                  << std::endl;
+        std::cout << "    Headers  : " << toString(record.headers())
+                  << std::endl;
+        std::cout << "    Key   [" << record.key().toString() << "]"
+                  << std::endl;
+        std::cout << "    Value [" << record.value().toString() << "]"
+                  << std::endl;
+      } else {
+        std::cerr << record.toString() << std::endl;
+      }
+    }
+  }
+
+  // No explicit close is needed, RAII will take care of it
+  consumer.close();
+}
