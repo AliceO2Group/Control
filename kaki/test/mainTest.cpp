cfb0f42d993b9dfed7f25ab29b5a5ae369f4867d
Michal Tichák <michal.tichak@cern.ch>
2024-02-22 17:41:43 +0100

    [kaki] added boost test

---
+++ kaki/test/mainTest.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE "O2 kaki unit tests"
+#include <boost/test/unit_test.hpp>
---
+++ kaki/test/simpleConsume.cpp
@@ -0,0 +1,3 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(simple_test) { BOOST_TEST(true); }
