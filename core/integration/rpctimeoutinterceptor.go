b14ce6b5ee7e60d28770644e0370dcaa97d597c3
Michal Tichák <michal.tichak@cern.ch>
2024-10-18 19:05:01 +0200

    [core] ddsched plugin gRPC calls have timeout by default

---
+++ core/integration/rpctimeoutinterceptor.go
@@ -0,0 +1,58 @@
+/*
+ * === This file is part of ALICE O² ===
+ *
+ * Copyright 2021 CERN and copyright holders of ALICE O².
+ * Author: Teo Mrnjavac <teo.mrnjavac@cern.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this license CERN does not waive the privileges and
+ * immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+package integration
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+func UnaryTimeoutInterceptor(timeout time.Duration, cause string) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		connection *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		// Create a context with a timeout
+		ctx, cancel := context.WithTimeoutCause(ctx, timeout, errors.New(cause))
+		defer cancel() // Ensure the context is canceled after the call
+
+		// Invoke the RPC call with the new context
+		err := invoker(ctx, method, req, reply, connection, opts...)
+		if err != nil {
+			st, _ := status.FromError(err)
+			// Handle error, maybe logging or processing the error status
+			return st.Err()
+		}
+		return nil
+	}
+}
